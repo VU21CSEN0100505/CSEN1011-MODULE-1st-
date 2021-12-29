c50d9cb1f4dbc1f694cc2b4efc5de2ec5c63dfaa
Dheeraj Uppada <93367489+DheerajSrivatsaUppada@users.noreply.github.com>
2021-12-29 23:52:01 +0530

    Add files via upload

---
+++ simplecalculatorsa.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main()
+{
+    char op;
+    int num1, num2, result;
+
+    scanf("%d %d %c", &num1,&num2,&op);
+
+
+    switch(op)
+    {
+        case '+': 
+            result = num1 + num2;
+            printf("%d",result);
+            break;
+
+        case '-': 
+            result = num1 - num2;
+            printf("%d",result);
+            break;
+
+        case '*': 
+            result = num1 * num2;
+            printf("%d",result);
+            break;
+
+        case '/': 
+            result = num1 / num2;
+            printf("%d",result);
+            break;
+        
+         case '%': 
+            result = num1 % num2;
+            printf("%d",result);
+            break;
+        default: 
+            printf("Invalid operator");
+    }
+
+    return 0;
+}
+
+
+/* Output:
+Run - 1
+8
+4
+4
+2
+
+
+Run - 2
+9
+3
+3
+27              */
+
+
+
