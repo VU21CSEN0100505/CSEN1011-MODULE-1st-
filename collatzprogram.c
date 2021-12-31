7472b3c422fd69e29943b5c48b54d38f30a3779e
Dheeraj Uppada <93367489+DheerajSrivatsaUppada@users.noreply.github.com>
2021-12-31 11:25:13 +0530

    Create collatzprogram.c

---
+++ collatzprogram.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n>1)
+    {
+        if(n%2 == 0)
+        {
+            n=n/2;
+            printf("%d\n",n);
+        }
+        else{
+            n = n * 3 + 1;
+            printf("%d\n",n);
+        }
+    }
+    return 0;
+}
